316c619f1be5bc0f439bb012a39db9e3dfab634f
adamKachmar <adam.kachmar@robolyon.com>
2025-08-04 18:25:59 +0200

    test 2

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../extensions'))
 # -- Project information -----------------------------------------------------
 
 project = 'Cours-WpiLib'
-copyright = "2025, Robo'Lyon"
+copyright = "2019, Robo'Lyon"
 author = "Robo'Lyon"
 
 # Name of "Master" document
